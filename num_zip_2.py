4473adb758b8e839f64033605f1778a282d859f0
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-03 15:41:03 -0400

    modified .py file.

--- num_zip_2.py
+++ num_zip_2.py
@@ -2,7 +2,8 @@ num_list_1 = [1, 2, 3, 4, 5]
 num_list_2 = [6, 7]
 
 def num_2(num_list_2):
-    for num in range(len(num_list_2)+(len(num_list_1)-(len(num_list_2)))):
+    for num in range(len(num_list_2)+ \
+                     (len(num_list_1)-(len(num_list_2)))):
         if num_list_2[num] < num_list_2[-1]:
             num_list_2.append(num_list_2[num]+2)
     return num_list_2
